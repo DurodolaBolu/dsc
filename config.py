043372c76b842a156488dcbc2da02c815d3e5887
BoluDurodola <Boluwatifedurodola@yahoo.com>
2020-09-20 23:43:37 +0100

    new changes

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ import os
 logging.basicConfig(level=logging.INFO)
 
 def create_api():
-    consumer_key = os.environ["CONSUMER_KEY"]
+    consumerkey = os.environ["CONSUMER_KEY"]
     consumer_secret = os.environ["CONSUMER_SECRET"]
     access_token = os.environ["ACCESS_TOKEN"]
     access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth = tweepy.OAuthHandler(consumerkey, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth, wait_on_rate_limit=True, 
         wait_on_rate_limit_notify=True)
